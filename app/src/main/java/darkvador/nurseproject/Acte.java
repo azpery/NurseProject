aa3c6561bd75201e44d49f7337137d3dcfa1d2e6
Stripote <matclemp@hotmail.fr>
2015-03-26 16:07:40 +0100

    L'affichage des actes devrait fonctionner. J'ai créé ActImportActe, chargée de récupérer sur alwaysdata les tuples dans la table 'soins'. Probleme avec RetourImport de ActImportActe, le StringBuilder ne semble pas récupérer le JSON de Async.
    Le reste est toujours fonctionnel.

--- app/src/main/java/darkvador/nurseproject/Acte.java
+++ app/src/main/java/darkvador/nurseproject/Acte.java
@@ -4,44 +4,45 @@ package darkvador.nurseproject;
  * Created by Mathieu on 25/03/2015.
  */
 public class Acte {
+    private int id;
+    private String libelle;
+
+    public boolean isEstFait() {
+        return estFait;
+    }
+
+    public void setEstFait(boolean estFait) {
+        this.estFait = estFait;
+    }
+
     public int getId() {
         return id;
-    }
-
-    public String getLibelle() {
-        return libelle;
-    }
-
-    public int getDuree() {
-        return duree;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
+    public String getLibelle() {
+        return libelle;
+    }
+
     public void setLibelle(String libelle) {
         this.libelle = libelle;
     }
 
-    public void setDuree(int duree) {
-        this.duree = duree;
+    public String getTypeSoin() {
+        return typeSoin;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setTypeSoin(String typeSoin) {
+        this.typeSoin = typeSoin;
     }
 
-    private int id;
-    private String libelle;
-    private int duree;
-    private String description;
-    public Acte(int unId, String unLibelle, int uneDuree, String uneDescription)
+    private String typeSoin;
+    private boolean estFait;
+    public Acte(int unId, String unLibelle, String unTypeSoin)
     {
-        id = unId; libelle = unLibelle; duree = uneDuree; description = uneDescription;
+        id = unId; libelle = unLibelle; typeSoin = unTypeSoin; estFait = false;
     }
 }
